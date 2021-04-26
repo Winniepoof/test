5c1f372076824f7675986071424a64aaf7fe710e
Winniepoof <1987769270@qq.com>
2021-04-26 10:18:44 +0800

    爬贴吧图片存数据库

---
+++ 爬图片存数据库/pipelines.py
@@ -0,0 +1,22 @@
+
+import sqlite3
+
+
+def process_item(item):
+    con=sqlite3.connect('d:/GitHub/testspider.db')
+    cur=con.cursor()
+
+    sql = 'insert into url(pdata) values(?)'
+
+    try:
+        cur.execute(sql,(item,))
+        con.commit()
+        print('插入成功')
+    except Exception as e:
+        print(e)
+        con.rollback()
+        print('插入失败')
+    finally:
+        cur.close()
+        con.close()
+
